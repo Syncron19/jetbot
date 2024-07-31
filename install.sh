b116b67ed13696005fb8e1cbe16251e674891510
Syncron19 <subir5372@gmail.com>
2024-07-31 03:20:45 -0400

    installation script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Preliminaries
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install python3-pip -y
+wget https://bootstrap.pypa.io/pip/3.6/get-pip.py
+python3 get-pip.py
+
+
+# jetson-inference installation (also installs PyTorch)
+sudo apt-get update
+sudo apt-get install git cmake libpython3-dev python3-numpy -y
+git clone --recursive --depth=1 https://github.com/dusty-nv/jetson-inference
+cd jetson-inference
+mkdir build
+cd build
+cmake -DENABLE_NVMM=OFF ../
+make -j2
+sudo make install
+sudo ldconfig
+
+
+# Other packages
+yes | sudo pip3 uninstall Pillow
+pip3 install Pillow==8.0.1
+pip3 install --upgrade setuptools
+pip3 install pyzmq
+pip3 install keyboard inputs traitlets packaging ipywidgets
+cd $HOME
+git clone https://github.com/NVIDIA-AI-IOT/torch2trt
+cd torch2trt
+sudo python3 setup.py install
+
+echo "export PYTHONPATH=$PYTHONPATH:/home/jetbot/jetbot" >> ~/.bashrc
